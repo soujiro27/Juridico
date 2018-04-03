f9796289bdd73efc3eaab7ec89d70fa39332c9a7
soujiro27 <jc.raus27@gmail.com>
2018-04-03 09:01:43 -0500

    haciendo el modal final

--- Dev/src/Work/Catalogos/Insert-Acciones.js
+++ Dev/src/Work/Catalogos/Insert-Acciones.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import { Form } from 'react-validify';
 import { BaseForm} from 'react-validify'
 import axios from 'axios';
+import Modal from 'react-responsive-modal';
 
 import Input from './../../form/Components/text'
 import './formAcciones.styl'
 export default class InsertAcciones extends Component {
 
+    state = {
+        open:false
+    }
+    
+    HandleModal = () => { 
+        this.setState({
+            open:!this.state.open
+        })
+    }
 
     submit(values){
+        //let self = this.bind(this)
 
         let form = new FormData()
         form.append('nombre',values.nombre)
         axios.post('/SIA/juridico/Acciones/Save',form)
         .then(response => {
-            console.log(response)
+            this.HandleModal()
         })
     }
 
@@ -42,10 +53,17 @@ export default class InsertAcciones extends Component {
                     submit
                     value="Guardar"
                     className="btn btn-primary btn-sm"
-                    onClick={this.submit}
+                    onClick={this.submit.bind(this)}
                 />
                 <a href={'/SIA/juridico/'+this.props.url} className ="btn btn-danger btn-sm">Cancelar</a>
+                <Modal open={this.state.open} 
+                    onClose={this.HandleModal} little
+                    closeOnEsc={false}
+                    closeOnOverlayClick={false}>
+                    <h2>Simple centered modal</h2>
+                </Modal>
             </Form>
+            
         )
     }
 }
